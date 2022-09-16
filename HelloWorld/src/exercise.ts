134cb5f8eea0db3c2cf4173430710a316e6418c3
Tomas Kaiser <tomas.kaiser06@gmail.com>
2022-09-16 21:59:31 +0100

    Add Property Decorators

--- HelloWorld/src/exercise.ts
+++ HelloWorld/src/exercise.ts
@@ -1,16 +1,28 @@
-function Capitalize(target: any, methodName: string, descriptor: PropertyDecorator) {
-    const original = descriptor.get;
-    descriptor.get = function() {
-       const result = original?.call(this);
-       return (typeof result === 'string') ? result.toLocaleUpperCase : result;
+function MinLength(length: number) {
+    return (target: any, propertyName: string) => {
+        let value: string;
+
+        const descriptor: PropertyDescriptor = {
+            get() { return value },
+            
+            set(newValue: string) {
+                if (newValue.length < length)
+                    throw new Error(`${propertyName} should be at least ${length} characters long.`);
+                value = newValue;
+            }
+        }
+
+        Object.defineProperty(target, propertyName, descriptor);
     }
 }
 
-class Person {
-    constructor(public firstName: string, public lastName: string) {}
+class User {
+    @MinLength(4)
+    password: string;
 
-    @Capitalize
-    get fullName() {
-        return `${this.firstName} ${this.lastName}`
+    constructor(passwrod: string) {
+        this.password = passwrod;
     }
 }
+
+let user = new User("abc"); // should throw error 
--- README.md
+++ README.md
@@ -1015,3 +1015,36 @@ class Person {
 let person = new Person("tom", "k");
 console.log(person.fullName); // Output: TOM K
 ```
+
+### Property Decorators
+
+```Typescript
+function MinLength(length: number) {
+    return (target: any, propertyName: string) => {
+        let value: string;
+
+        const descriptor: PropertyDescriptor = {
+            get() { return value },
+            
+            set(newValue: string) {
+                if (newValue.length < length)
+                    throw new Error(`${propertyName} should be at least ${length} characters long.`);
+                value = newValue;
+            }
+        }
+
+        Object.defineProperty(target, propertyName, descriptor);
+    }
+}
+
+class User {
+    @MinLength(4)
+    password: string;
+
+    constructor(passwrod: string) {
+        this.password = passwrod;
+    }
+}
+
+let user = new User("abc"); // should throw error 
+```
